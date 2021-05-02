09edac865fa3244e7b289dcc72fd5664c2c37ef6
taarakdhingra <46484930+taarakdhingra@users.noreply.github.com>
2021-05-02 17:43:04 +1000

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -35,13 +35,13 @@ def xml_to_csv(path):
             classes_names.append(member[0].text)
             value = (
                 root.find("filename").text,
-                int(root.find("size")[0].text),
-                int(root.find("size")[1].text),
+                float(root.find("size")[0].text),
+                float(root.find("size")[1].text),
                 member[0].text,
-                int(member[2][0].text),
-                int(member[2][1].text),
-                int(member[2][2].text),
-                int(member[2][3].text),
+                float(member[4][0].text),
+                float(member[4][1].text),
+                float(member[4][2].text),
+                float(member[4][3].text),
             )
             xml_list.append(value)
     column_name = [
